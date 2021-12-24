1a35976f367b22257d4e4ce4124a75d91dc08114
Kachouti Mohamed <kachouti36@gmail.com>
2021-12-24 17:09:59 +0100

    push to the start

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,27 +1,11 @@
 package fr.lernejo.navy_battle;
 
 import com.sun.net.httpserver.HttpServer;
-import fr.lernejo.navy_battle.Partie.Jeu;
-import fr.lernejo.navy_battle.Partie.Joueur;
-import fr.lernejo.navy_battle.Utils.PostRequest;
+
 import org.apache.commons.validator.routines.UrlValidator;
 
 public class Launcher {
     public static void main(String[] args) throws Exception {
-        if (args.length < 1) {System.out.println("enrez le port!"); System.exit(1);}
-        try{
-            final int port = Integer.parseInt(args[0]);
-            final Joueur joueur = new Joueur();
-            final  Jeu jeu = new Jeu(joueur);
-           final  HttpServer httpServer = new Serveur().launch(port, jeu); httpServer.start();
-            jeu.jeuD();
-            if (args.length > 1){
-                UrlValidator urlValidator = new UrlValidator(UrlValidator.ALLOW_LOCAL_URLS);
-                if(!urlValidator.isValid(args[1])){ System.out.println("Adversary's URL " + args[1] + " isn't valid !");
-                    System.exit(1);}
-                jeu.joueur.adversaryURL[0] = args[1];
-                PostRequest postRequest = new PostRequest(port); postRequest.sendPostRequest(args[1]);
-            }
-        } catch (Exception e){throw new NumberFormatException(e.getMessage());}
+
     }
 }
--- src/main/java/fr/lernejo/navy_battle/Serveur.java
+++ src/main/java/fr/lernejo/navy_battle/Serveur.java
@@ -2,9 +2,6 @@ package fr.lernejo.navy_battle;
 
 import com.sun.net.httpserver.HttpServer;
 import fr.lernejo.navy_battle.Handler.CallHandler;
-import fr.lernejo.navy_battle.Handler.FireHandler;
-import fr.lernejo.navy_battle.Handler.PostHandler;
-import fr.lernejo.navy_battle.Partie.Jeu;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -12,11 +9,9 @@ import java.util.concurrent.Executors;
 
 public class Serveur {
 
-    public HttpServer launch(int port, Jeu jeu) throws IOException {
+    public HttpServer launch(int port) throws IOException {
         final HttpServer httpServer = HttpServer.create(new InetSocketAddress(port), 0);
         httpServer.createContext("/ping", new CallHandler());
-        httpServer.createContext("/api/game/start", new PostHandler(jeu));
-        httpServer.createContext("/api/game/fire", new FireHandler(jeu));
         httpServer.setExecutor(Executors.newFixedThreadPool(1));
 
         return httpServer;
